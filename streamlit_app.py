7e1c2a462ffaacf618d9ce27b8bdb87f71be73c7
CodeWiz <vanshi.stu@gmail.com>
2024-05-23 18:04:00 +0530

    V1

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Login page",
+    page_icon="👋",
+    layout="wide",
+)
+
+st.sidebar.image("https://raw.githubusercontent.com/CodeWiza/PriceBenchmarking-Dashboard/main/logo.png", use_column_width=True)
